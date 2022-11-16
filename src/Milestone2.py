5a4f8988e1e3bf1b49d05281cc843369df0d525c
Marno <marnonel2023@u.northwestern.edu>
2022-11-15 19:33:59 -0600

    End effector performs the whole pick and place traj

--- src/Milestone2.py
+++ src/Milestone2.py
@@ -53,10 +53,15 @@ Tse_initial = np.array([[ 0, 0, 1,  0 ],
 #                          [ 0, 0,-2,            0.3   ],
 #                          [ 0, 0, 0,             1    ]])
 angle_grasp = 1.785398
-Tce_standoff = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0 ],
-                         [          0,          1,            0,          0 ],
-                         [-np.sin(angle_grasp), 0,  np.cos(angle_grasp), 0.3],
-                         [          0,          0,             0,         1 ]])
+Tce_standoff_i = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0 ],
+                           [          0,          1,            0,            0    ],
+                           [-np.sin(angle_grasp), 0,  np.cos(angle_grasp),   0.3   ],
+                           [          0,          0,             0,           1    ]])
+
+Tce_standoff_e = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  0 ],
+                           [          0,          1,            0,            0    ],
+                           [-np.sin(angle_grasp), 0,  np.cos(angle_grasp),   0.3   ],
+                           [          0,          0,             0,           1    ]])
 # End effector configuration relative to cube when grasp,
 # Add d2/2 to have cube in centre of gripper
 # Tce_grasp = np.array([[ 0, 0, 1, Cube_i[0]+d2/2.0],
@@ -67,7 +72,12 @@ Tce_standoff = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0
 #                       [ 0, 1, 0, 0],
 #                       [-1, 0, 0, 0],
 #                       [ 0, 0, 0, 1]])
-Tce_grasp = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0 ],
+Tce_grasp_i = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0 ],
+                      [          0,          1,            0,          0 ],
+                      [-np.sin(angle_grasp), 0,  np.cos(angle_grasp),  0 ],
+                      [          0,          0,             0,         1 ]])
+
+Tce_grasp_e = np.array([[ np.cos(angle_grasp), 0,  np.sin(angle_grasp),  d2/2.0  ],
                       [          0,          1,            0,          0 ],
                       [-np.sin(angle_grasp), 0,  np.cos(angle_grasp),  0 ],
                       [          0,          0,             0,         1 ]])
@@ -79,8 +89,13 @@ def Traj_specs(Tf):
     trajectory_mat = np.zeros((N,13))
     return N, trajectory_mat
 
+def append_to_csv(trajectory_mat):
+    """Appends trajectory_mat to an existing csv file."""
+    with open("Milestone1.csv",'a') as csvfile:
+        np.savetxt(csvfile, trajectory_mat, delimiter = ",")
 
-def TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select):
+
+def TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp):
     """
     Trajectory generator function
     """
@@ -101,6 +116,8 @@ def TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select
             f = 9+j
             trajectory_mat[i,f] = traj[i][j,3] # px, py, pz
             m += 3 # Scale by one row
+        trajectory_mat[i,12] = grasp # Set Gripper
+
     return trajectory_mat
 
 
@@ -112,34 +129,151 @@ method = 5
 
 
 """
-Traj 1
+Traj 1 - Initial to standoff initial
 """
 Tf = 3 # Amount of time for motion
 N, trajectory_mat = Traj_specs(Tf)
+grasp = 0 # Open gripper
 
 Xstart = Tse_initial
-Xend = Tsc_initial@Tce_standoff
+Xend = Tsc_initial@Tce_standoff_i
 traj_select = 1 # Select screw trajectory
 
-trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select)
-# Overwrite csv file
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select,grasp)
+
+# Create csv file
 np.savetxt("Milestone1.csv", trajectory_mat, delimiter = ",")
 
 
 """
-Traj 2
+Traj 2 - standoff initial to grasp initial
 """
 Tf = 3 # Amount of time for motion
 N, trajectory_mat = Traj_specs(Tf)
+grasp = 0 # Open gripper
 
-Xstart = Tsc_initial@Tce_standoff
-Xend = Tsc_initial@Tce_grasp
+Xstart = Tsc_initial@Tce_standoff_i
+Xend = Tsc_initial@Tce_grasp_i
 traj_select = 0 # Cartesian trajectory
 
 # print(f"\n {Tsc_initial@Tce_standoff}")
 
-trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select)
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
 
 # Append to csv file
-with open("Milestone1.csv",'a') as csvfile:
-    np.savetxt(csvfile, trajectory_mat, delimiter = ",")
+append_to_csv(trajectory_mat)
+
+
+"""
+Traj 3 - Grasp initial
+"""
+Tf = 1 # Wai for 1sec to grab block
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 1 # Close gripper
+
+Xstart = Tsc_initial@Tce_grasp_i
+Xend = Tsc_initial@Tce_grasp_i
+traj_select = 0 # Cartesian trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
+
+
+"""
+Traj 4 - Grasp initial to standoff initial
+"""
+Tf = 3 # Amount of time for motion
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 1 # Close gripper
+
+Xstart = Tsc_initial@Tce_grasp_i
+Xend = Tsc_initial@Tce_standoff_i
+traj_select = 0 # Cartesian trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
+
+
+"""
+Traj 5 - Standoff initial to standoff goal
+"""
+Tf = 5 # Amount of time for motion
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 1 # Close gripper
+
+Xstart = Tsc_initial@Tce_standoff_i
+Xend = Tsc_goal@Tce_standoff_e
+traj_select = 1 # Screw trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
+
+
+"""
+Traj 6 - Standoff goal to grasp goal
+"""
+Tf = 3 # Amount of time for motion
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 1 # Close gripper
+
+Xstart = Tsc_goal@Tce_standoff_e
+Xend = Tsc_goal@Tce_grasp_e
+traj_select = 0 # Cartesian trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
+
+
+
+"""
+Traj 7 - Grasp goal - Release block
+"""
+Tf = 1 # Amount of time for motion
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 0 # Close gripper
+
+Xstart = Tsc_goal@Tce_grasp_e
+Xend = Tsc_goal@Tce_grasp_e
+traj_select = 0 # Cartesian trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
+
+
+"""
+Traj 8 - Grasp goal to Standoff goal
+"""
+Tf = 3 # Amount of time for motion
+N, trajectory_mat = Traj_specs(Tf)
+grasp = 0 # Close gripper
+
+Xstart = Tsc_goal@Tce_grasp_e
+Xend = Tsc_goal@Tce_standoff_e
+traj_select = 0 # Cartesian trajectory
+
+# print(f"\n {Tsc_initial@Tce_standoff}")
+
+trajectory_mat = TrajectoryGenerator(Xstart, Xend, Tf, N, method, trajectory_mat, traj_select, grasp)
+
+# Append to csv file
+append_to_csv(trajectory_mat)
